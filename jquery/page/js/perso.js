e4bafb77ac341cf9539c6a124381c7f46f4c0076
saharasil <58476824+saharasil@users.noreply.github.com>
2020-02-20 06:42:25 +0100

    commit

--- jquery/page/js/perso.js
+++ jquery/page/js/perso.js
@@ -1,74 +1,17 @@
 // mon js pour ma page slideshow.html 
 $(function(){
 
-  
     $('aside a').click(function(event){
         // alert('js ok');
         //j'annule le clic sur le lien
         event.preventDefault();
         // console.log($(this).attr('href'));
         var moHref = $(this).attr('href');
-        $('main').find('img').attr('src',moHref); 
+        $('main').find('img').attr('src', moHref); 
         
-
+        $('aside a').click(function(){
+            console.log($(this).attr('title'));
+            var monAlt = $(this).attr('title');
+            $('main').find('figcaption h3').text(monAlt);});
     });
-
-    // a revoir 
-    $('aside ~ a').click(function(){
-        
-        var monAlt = $('aside ~ a+img').attr('alt');
-        console.log($('aside a img').attr('alt'));
-        $('main').find('img').attr('alt');
-       
-    });
-
-   
-});
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ });
